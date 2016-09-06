8bd00d58ee9eff5e5590bbf90725686dc388dd61
Daniel Lennox <dlennox2424@gmail.com>
2016-09-06 14:32:52 -0700

    Init. Adding files with basic PA1 stucture

---
+++ histogram.h
@@ -0,0 +1,14 @@
+#ifndef HISTOGRAM_H_INCLUDE
+#define HISTOGRAM_H_INCLUDE
+
+#include<iostream>
+#include<fstream>
+using std::istream;
+using std::ostream;
+
+class Histogram{
+public:
+   
+}
+
+#endif // HISTOGRAM_H_INCLUDE
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <histogram.h>
+#include <iostream>
+using std::cout;
+using std::endl;
+#include <fstream>
+using std::ifstream;
+
+int main(int argc, char* argv[]){
+
+}
