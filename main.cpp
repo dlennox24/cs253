360252a04083a2fd4965af1f2e608b3d87bab145
Daniel Lennox <dlennox2424@gmail.com>
2016-09-06 15:03:51 -0700

    Testing main.ccp read file

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 #include <histogram.h>
 #include <iostream>
 using std::cout;
+using std::cerr;
 using std::endl;
 #include <fstream>
 using std::ifstream;
 
 int main(int argc, char* argv[]){
+   if(argc != 2){
+      cerr << "Invalid number of arguments!" << endl;
+      cerr << "Usage: " << arg0 << " filename" << endl;
+      return -1;
+   }
 
+   ifstream istr(argv[1]);
+   if(istr.fail()){
+      cerr << "Error reading file: "<< argv[1] << endl;
+      return -1;
+   }
+
+   return 0;
 }
