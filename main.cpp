5c4a16000a28585ef97363da69dd79ffa690a17f
Daniel Lennox <dlennox2424@gmail.com>
2016-09-06 16:33:35 -0700

    testing range errors

--- main.cpp
+++ main.cpp
@@ -14,6 +14,13 @@ int main(int argc, char* argv[]){
       return -1;
    }
 
+   string filename(argv[1]);
+   string filetype(filename.substr(s.find("."),filename.length));
+   if(filetype.compare("txt") != 0){
+      cerr << "File must be of the type .txt" << endl;
+      return -1;
+   }
+
    ifstream istr(argv[1]);
    if(istr.fail()){
       cerr << "Error reading file: "<< argv[1] << endl;
@@ -21,6 +28,7 @@ int main(int argc, char* argv[]){
    }
 
    int in;
+   bool emptyFile = true;
    while(true){
       istr >> in;
       if(istr.eof()){
@@ -28,15 +36,21 @@ int main(int argc, char* argv[]){
       }
 
       if(istr.fail()){
-         cerr << in << "All values must be integers!" << endl;
+         cerr << in << ": All values must be integers!" << endl;
          return -1;
       }else if(in > 255 || in < 0){
          cerr << in << ": All values must be in the range 0-255!" << endl;
          return -1;
       }else{
+         emptyFile = false;
          cout << "Value: [" << in << "]" << endl;
          cout << "-----------" << endl;
       }
+   }
+
+   if(emptyFile){
+      cerr << "File empty!" << endl;
+      return -1;
    }
 
    return 0;
